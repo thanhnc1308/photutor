bd58ab920a2de71a649dc38b236fa045257c1226
thanhnc <thanhnc138@mail.com>
2021-10-24 20:51:12 +0700

    update log

--- server/application/core/BaseLogger.py
+++ server/application/core/BaseLogger.py
@@ -1,38 +1,43 @@
-from loguru import logger
+# from loguru import logger
+import logging
 import inspect
-import sys
+from application.core.Singleton import Singleton
+MAX_COUNT_LOG = 1000
 
 
-class BaseLogger:
-    count = 0
+class BaseLogger(metaclass=Singleton):
+    exception_count = 0
 
     def __init__(self):
-        self.count += 1
-        logger.add(sys.stdout, colorize=True, format="<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level: "
-                                                     "<8}</level> | <level>{message}</level>")
-        print(f"Init BaseLogger: {self.count}")
+        logging.basicConfig(format="%(asctime)s - %(levelname)s - %(message)s")
+
+    def process_log(self, log_level, msg):
+        # TODO: create a new thread to not affect the current request
+        try:
+            prefix = self.get_prefix()
+            logging.log(log_level, f"{prefix} | {msg}")
+        except Exception as e:
+            # limit the log time
+            if self.exception_count < MAX_COUNT_LOG:
+                self.exception(str(e))
+            self.exception_count += 1
 
     def debug(self, msg, data={}):
-        prefix = self.get_pre_fix()
-        logger.debug(f"{prefix} | {msg}")
+        self.process_log(logging.DEBUG, msg)
 
     def info(self, msg, data={}):
-        prefix = self.get_pre_fix()
-        logger.info(f"{prefix} | {msg}")
+        self.process_log(logging.INFO, msg)
 
     def warning(self, msg, data={}):
-        prefix = self.get_pre_fix()
-        logger.warning(f"{prefix} | {msg}")
+        self.process_log(logging.WARNING, msg)
 
     def error(self, msg, data={}):
-        prefix = self.get_pre_fix()
-        logger.error(f"{prefix} | {msg}")
+        self.process_log(logging.ERROR, msg)
 
     def exception(self, msg, data={}):
-        prefix = self.get_pre_fix()
-        logger.exception(f"{prefix} | {msg}")
+        self.process_log(logging.CRITICAL, msg)
 
-    def get_pre_fix(self):
+    def get_prefix(self):
         last_stack_call = inspect.stack()[2]
         func = last_stack_call.function
         filename = last_stack_call.filename
